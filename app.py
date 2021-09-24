c5230753081d4bd41dc93bb5af80493b5a5e5a95
Ankit Singh <63934421+ankit-21@users.noreply.github.com>
2021-09-24 11:16:25 -0400

    Updated Version of the Python Backend

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 # importing necessary libraries
+import plotly.utils
 from flask import Flask, render_template, request, url_for, redirect, session
 from flask_session import Session
 import pandas as pd
 from sys import platform
+import plotly.graph_objects as go
+import json
 
 # defining the app name and session configuration to store variables in the session
 app = Flask(__name__)
@@ -10,6 +13,81 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
+def fish_sankey(
+        TEST_NAME,
+        fused,
+        split,
+        isolated3,
+        isolated5):
+    '''
+    Takes test name and FISH counts to produce a Sankey diagram.
+    Default values are for example case
+    '''
+
+    iso5_green = ['ROS1', 'ALK', 'CHOP', 'FKHR', 'NTRK1', 'NTRK3', 'GENE_GREEN']
+
+    # set correct 3'/5' colors
+    if TEST_NAME in iso5_green:
+        iso3_color, iso5_color = "red", "green"
+    else:
+        iso3_color, iso5_color = "green", "red"
+
+    # # do simple calculations for later display
+    add = split + fused + isolated3 + isolated5
+    fused_perc = round(100 * (fused / add))
+    split_perc = round(100 * (split / add))
+    iso5_perc = round(100 * (isolated5 / add))
+    iso3_perc = round(100 * (isolated3 / add))
+    non_fused = split + isolated5 + isolated3
+    non_fused_perc = round(100 * (non_fused / add))
+
+    # set node labels - unfortunately cannot add line breaks - it's a known bug
+    labels = [f"Total Counted: {add}",
+              f"Non-Fused: {non_fused}/{add} ({non_fused_perc}%)",
+              f"Fused: {fused}/{add} ({fused_perc}%)",
+              f"Split: {split}/{add} ({split_perc}%)",
+              f"Isolated 3': {isolated3}/{add} ({iso3_perc}%)",
+              f"Isolated 5': {isolated5}/{add} ({iso5_perc}%)"]
+
+    # set colors for nodes
+    color = ["black", "blue", "orange", "white", iso3_color, iso5_color]
+
+    #  %% Draw the Sankey
+    fig = go.Figure(data=[go.Sankey(
+        valueformat=".0f",
+        arrangement="snap",
+        node=dict(
+            pad=15,
+            thickness=20,
+            line=dict(color="black", width=0.5),
+            label=labels,
+            x=[0.01, .5, .5, .99, .99, .99],
+            y=[.01, .01, .9, .01, .01, .01],
+            color=color
+        ),
+        link=dict(
+            source=[0, 0, 1, 1, 1],  # indices correspond to labels above
+            target=[1, 2, 3, 4, 5],
+            value=[non_fused, fused, split, isolated3, isolated5]
+        ))])
+
+    fig.update_layout(
+        font_size=8,
+        autosize=False,
+        width=600,
+        height=400,
+        margin=dict(
+            l=50,
+            r=50,
+            b=100,
+            t=100,
+            pad=4
+        ))
+
+    # %% Return the figure
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
 
 @app.route("/")
 def dropdown():
@@ -49,6 +127,7 @@ def nomenc():
     global iso3_perc
     global non_fused
     global non_fused_perc
+    global plotly_plot
 
     # parsing the test name stored in sessions earlier
     TEST_NAME = session['TEST_NAME']
@@ -82,8 +161,11 @@ def nomenc():
         isolated5 = 0
         isolated3 = 0
 
+        iso5_green = ['ROS1', 'ALK', 'CHOP', 'FKHR', 'NTRK1', 'NTRK3', 'GENE_GREEN']
+        iso5_red = ['MYC', 'BCL2', 'BCL6', 'RET', 'EWSR1', 'SYT', 'GENE_RED']
+
         # 5' Green and 3' Red
-        if TEST_NAME == 'ROS1' or TEST_NAME == 'ALK' or TEST_NAME == 'CHOP' or TEST_NAME == 'FKHR' or TEST_NAME == 'NTRK1' or TEST_NAME == 'NTRK3' or TEST_NAME == "GENE_GREEN":
+        if TEST_NAME in iso5_green:
             for i in range(df.shape[0]):
                 if df['Red'].loc[i] > 0 and df['Green'].loc[i] > 0 and df['Yellow'].loc[i] >= 0:
                     split += 1  # adding 1 to the split case
@@ -206,7 +288,7 @@ def nomenc():
                 iso3 = ""
 
         # 5' Red and 3' Green
-        elif TEST_NAME == 'MYC' or TEST_NAME == 'BCL2' or TEST_NAME == 'BCL6' or TEST_NAME == 'RET' or TEST_NAME == 'EWSR1' or TEST_NAME == 'SYT' or TEST_NAME == "GENE_RED":
+        elif TEST_NAME in iso5_red:
             for i in range(df.shape[0]):
                 if df['Red'].loc[i] > 0 and df['Green'].loc[i] > 0 and df['Yellow'].loc[i] >= 0:
                     split += 1  # adding 1 to the split case
@@ -339,87 +421,85 @@ def nomenc():
         d = a_sorted_keys[3]
 
         if a == "split":
-            nom1 = splt 
+            nom1 = '<p style="display:inline-block">' + splt + "</p>"
         elif a == "fused":
-            nom1 = fsd
+            nom1 = '<p style="display:inline-block;color:orange">' + fsd + "</p>"
         elif a == "isolated5":
-            nom1 = iso5
+            if TEST_NAME == 'ROS1' or TEST_NAME == 'ALK' or TEST_NAME == 'CHOP' or TEST_NAME == 'FKHR' or TEST_NAME == 'NTRK1' or TEST_NAME == 'NTRK3' or TEST_NAME == "GENE_GREEN":
+                nom1 = '<p style="display:inline-block;color:green">' + iso5 + "</p>"
+            else:
+                nom1 = '<p style="display:inline-block;color:red">' + iso5 + "</p>"
         elif a == "isolated3":
-            nom1 = iso3
+            if TEST_NAME == 'ROS1' or TEST_NAME == 'ALK' or TEST_NAME == 'CHOP' or TEST_NAME == 'FKHR' or TEST_NAME == 'NTRK1' or TEST_NAME == 'NTRK3' or TEST_NAME == "GENE_GREEN":
+                nom1 = '<p style="display:inline-block;color:red">' + iso3 + "</p>"
+            else:
+                nom1 = '<p style="display:inline-block;color:green">' + iso3 + "</p>"
 
         if b == "split":
-            nom2 = splt
+            nom2 = '<p style="display:inline-block">' + splt + "</p>"
         elif b == "fused":
-            nom2 = fsd
+            nom2 = '<p style="display:inline-block;color:orange">' + fsd + "</p>"
         elif b == "isolated5":
-            nom2 = iso5
+            if TEST_NAME == 'ROS1' or TEST_NAME == 'ALK' or TEST_NAME == 'CHOP' or TEST_NAME == 'FKHR' or TEST_NAME == 'NTRK1' or TEST_NAME == 'NTRK3' or TEST_NAME == "GENE_GREEN":
+                nom2 = '<p style="display:inline-block;color:green">' + iso5 + "</p>"
+            else:
+                nom2 = '<p style="display:inline-block;color:red">' + iso5 + "</p>"
         elif b == "isolated3":
-            nom2 = iso3
+            if TEST_NAME == 'ROS1' or TEST_NAME == 'ALK' or TEST_NAME == 'CHOP' or TEST_NAME == 'FKHR' or TEST_NAME == 'NTRK1' or TEST_NAME == 'NTRK3' or TEST_NAME == "GENE_GREEN":
+                nom2 = '<p style="display:inline-block;color:red">' + iso3 + "</p>"
+            else:
+                nom2 = '<p style="display:inline-block;color:green">' + iso3 + "</p>"
 
         if c == "split":
-            nom3 = splt
+            nom3 = '<p style="display:inline-block">' + splt + "</p>"
         elif c == "fused":
-            nom3 = fsd
+            nom3 = '<p style="display:inline-block;color:orange">' + fsd + "</p>"
         elif c == "isolated5":
-            nom3 = iso5
+            if TEST_NAME == 'ROS1' or TEST_NAME == 'ALK' or TEST_NAME == 'CHOP' or TEST_NAME == 'FKHR' or TEST_NAME == 'NTRK1' or TEST_NAME == 'NTRK3' or TEST_NAME == "GENE_GREEN":
+                nom3 = '<p style="display:inline-block;color:green">' + iso5 + "</p>"
+            else:
+                nom3 = '<p style="display:inline-block;color:red">' + iso5 + "</p>"
         elif c == "isolated3":
-            nom3 = iso3
+            if TEST_NAME == 'ROS1' or TEST_NAME == 'ALK' or TEST_NAME == 'CHOP' or TEST_NAME == 'FKHR' or TEST_NAME == 'NTRK1' or TEST_NAME == 'NTRK3' or TEST_NAME == "GENE_GREEN":
+                nom3 = '<p style="display:inline-block;color:red">' + iso3 + "</p>"
+            else:
+                nom3 = '<p style="display:inline-block;color:green">' + iso3 + "</p>"
 
         if d == "split":
-            nom4 = splt
+            nom4 = '<p style="display:inline-block">' + splt + "</p>"
         elif d == "fused":
-            nom4 = fsd
+            nom4 = '<p style="display:inline-block;color:orange">' + fsd + "</p>"
         elif d == "isolated5":
-            nom4 = iso5
+            if TEST_NAME == 'ROS1' or TEST_NAME == 'ALK' or TEST_NAME == 'CHOP' or TEST_NAME == 'FKHR' or TEST_NAME == 'NTRK1' or TEST_NAME == 'NTRK3' or TEST_NAME == "GENE_GREEN":
+                nom4 = '<p style="display:inline-block;color:green">' + iso5 + "</p>"
+            else:
+                nom4 = '<p style="display:inline-block;color:red">' + iso5 + "</p>"
         elif d == "isolated3":
-            nom4 = iso3
+            if TEST_NAME == 'ROS1' or TEST_NAME == 'ALK' or TEST_NAME == 'CHOP' or TEST_NAME == 'FKHR' or TEST_NAME == 'NTRK1' or TEST_NAME == 'NTRK3' or TEST_NAME == "GENE_GREEN":
+                nom4 = '<p style="display:inline-block;color:red">' + iso3 + "</p>"
+            else:
+                nom4 = '<p style="display:inline-block;color:green">' + iso3 + "</p>"
 
-        # adding all the nomenclature strings together
+        # adding all the nomenclature strings together and removing extra "/" for unused nom strings
         nom = "nuc ish"
-        Nom = nom + nom1 + "/" + nom2 + "/" + nom3 + "/" + nom4
-
-        # Removing extra "/" for unused nom strings
-        if Nom[-3] == "/":
-            Nom = Nom[:-3]
-        elif Nom[-2] == "/":
-            Nom = Nom[:-2]
-        elif Nom[-1] == "/":
-            Nom = Nom[:-1]
+        if nom1 == '<p style="display:inline-block;color:red"></p>' or nom1 == '<p style="display:inline-block;color:green"></p>' or nom1 == '<p style="display:inline-block;color:orange"></p>' or nom1 == '<p style="display:inline-block"></p>':
+            Nom = nom
+        elif nom2 == '<p style="display:inline-block;color:red"></p>' or nom2 == '<p style="display:inline-block;color:green"></p>' or nom2 == '<p style="display:inline-block;color:orange"></p>' or nom2 == '<p style="display:inline-block"></p>':
+            Nom = nom + " " + nom1
+        elif nom3 == '<p style="display:inline-block;color:red"></p>' or nom3 == '<p style="display:inline-block;color:green"></p>' or nom3 == '<p style="display:inline-block;color:orange"></p>' or nom3 == '<p style="display:inline-block"></p>':
+            Nom = nom + " " + nom1 + " / " + nom2
+        elif nom4 == '<p style="display:inline-block;color:red"></p>' or nom4 == '<p style="display:inline-block;color:green"></p>' or nom4 == '<p style="display:inline-block;color:orange"></p>' or nom4 == '<p style="display:inline-block"></p>':
+            Nom = nom + " " + nom1 + " / " + nom2 + " / " + nom3
         else:
-            Nom = Nom
+            Nom = nom + " " + nom1 + " / " + nom2 + " / " + nom3 + " / " + nom4
 
         # Cut-off for different tests
-        if TEST_NAME == 'ROS1':
-            cut = 'A ROS1 rearrangement is reported if more than 15% of cells show split signals.'
-        elif TEST_NAME == 'ALK':
-            cut = 'An ALK rearrangement is reported if more than 15% of cells show split signals.'
-        elif TEST_NAME == 'RET':
-            cut = 'A RET rearrangement is reported if more than 15% of cells show split signals.'
-        elif TEST_NAME == 'SYT':
-            cut = 'A SYT rearrangement is reported if more than 15% of cells show split signals.'
-        elif TEST_NAME == 'BCL2':
-            cut = 'A BCL2 rearrangement is reported if more than 15% of cells show split signals.'
-        elif TEST_NAME == 'BCL6':
-            cut = 'A BCL6 rearrangement is reported if more than 15% of cells show split signals.'
-        elif TEST_NAME == 'CHOP':
-            cut = 'A CHOP rearrangement is reported if more than 20% of cells show split signals.'
-        elif TEST_NAME == 'EWSR1':
-            cut = 'A EWSR1 rearrangement is reported if more than 15% of cells show split signals.'
-        elif TEST_NAME == 'FKHR':
-            cut = 'A FKHR rearrangement is reported if more than 15% of cells show split signals.'
-        elif TEST_NAME == 'MYC':
-            cut = 'A MYC rearrangement is reported if more than 15% of cells show split signals.'
-        elif TEST_NAME == 'NTRK1':
-            cut = 'A sample is classified as negative if <15% of nuclei (7 nuclei or fewer) are positive for gene rearrangement and classified as positive if >15% (8 nuclei or more) are positive. If more than 50 nuclei are enumerated, a sample is classified as positive if ≥15% of nuclei are positive.'
-        elif TEST_NAME == 'NTRK3':
-            cut = 'A sample is classified as negative if <15% of nuclei (7 nuclei or fewer) are positive for gene rearrangement and classified as positive if >15% (8 nuclei or more) are positive. If more than 50 nuclei are enumerated, a sample is classified as positive if ≥15% of nuclei are positive.'
-        elif TEST_NAME == 'GENE_RED':
-            cut = 'A GENE_RED rearrangement is reported if more than 15% of cells show split signals.'
-        elif TEST_NAME == 'GENE_GREEN':
-            cut = 'A GENE_GREEN rearrangement is reported if more than 15% of cells show split signals.'
-        else:
-            cut = ''
+        cut_off = ['ROS1', 'ALK', 'RET', 'SYT', 'BCL2', 'BCL6', 'EWSR1', 'FKHR', 'MYC', 'NTRK1', 'NTRK3', 'GENE_RED', 'GENE_GREEN']
 
+        if TEST_NAME in cut_off:
+            cut = 'A ROS1 rearrangement is reported if more than 15% of cells show split signals.'
+        else:
+            cut = 'A CHOP rearrangement is reported if more than 20% of cells show split signals.'
 
         # Obtaining macro for FISH report on WIKI
         out = str(df.to_csv(header=False, index=False))
@@ -429,11 +509,15 @@ def nomenc():
         else:
             # iOS or Linux
             out_final = out.replace("\n", ";")
+
+        # Sankey diagram
+        plotly_plot = fish_sankey(TEST_NAME=TEST_NAME, fused=fused, split=split, isolated3=isolated3, isolated5=isolated5)
+
         return redirect(url_for('nomenc'))
     return render_template('nom.html', Nom=Nom, out_final=out_final, TEST_NAME=TEST_NAME, add=add, nom1=nom1, nom2=nom2,
                            nom3=nom3, nom4=nom4, cut=cut, fused_perc=fused_perc, split_perc=split_perc,
                            iso5_perc=iso5_perc, iso3_perc=iso3_perc, non_fused_perc=non_fused_perc,
-                           non_fused=non_fused, fsd=fsd, splt=splt, iso3=iso3, iso5=iso5)
+                           non_fused=non_fused, fsd=fsd, splt=splt, iso3=iso3, iso5=iso5, plotly_plot=plotly_plot)
 
 
 if __name__ == "__main__":
